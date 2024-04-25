f81a6fe2812f485c7000408d934b4a5619d9dd19
SrikarHasthi <hsrikar1999@gmail.com>
2024-04-25 13:56:35 -0400

    new features msg

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -82,7 +82,7 @@ const Sidebar = ({ allTasksHistory }: Props) => {
                             <img src={question} alt="question" />
                         </span>
                         <Tooltip id="coming-soon-tooltip" data-tooltip-place="right-start" style={{ width: "13rem", background: "black" }}>
-                            Backend with Java Spring Boot with Login, Profile, Tasks History, Favourite tasks and many more..
+                            Favourite tasks and many more..
                             Stay Tuned
                         </Tooltip>
                     </p>
